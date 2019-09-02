b1833cd21f0b420b8c2a6a6dbab0dbcfe9de7061
Cyprian Bowen <branco1lelgo@gmail.com>
2019-09-02 08:32:24 +0300

    add the function for choosing the size of the pizza

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@ function pizza(size, crust, toppings){
     this.crust = crust
     this.toppings = toppings
 }
+pizza.prototype.sizePrice = function(){
+    if(this.size === "small"){
+        return 800
+    }else if(this.size === "medium"){
+        return 1000
+    }else if(this.zie === "large"){
+        return 1300
+    }else{
+        alert("Please select the size you would prefer")
+    }
+}
